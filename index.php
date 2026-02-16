454d1ee50236a7e404386256c15267ea7808c6c9
DJJ006 <danielsjj.006@gmail.com>
2026-02-16 13:14:05 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SkateShop</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+
+<header>
+    <a href="./" class="logo">
+        <i class="fas fa-clinic-medical"></i> Aptieka
+    </a>
+
+    
+
+            <nav>
+            <a class="btn" data-target="#modal-ticket">
+                <i class="fas fa-user-md"></i> Pieteikties konsultācijai
+            </a>
+            <a href="#buy-meds" class="btn">
+                <i class="fas fa-pills"></i> Iegādāties medikamentus
+            </a>
+            <a class="btn" id="cart-btn">
+                <i class="fas fa-shopping-cart"></i> Mans grozs (<span id="cart-count">0</span>)
+            </a>
+            
+        </nav>
+
+</header>
+    
+</body>
+</html>
